37ef0070b4f1fe9db97aad43acaec214be11331b
Steven <Stevendiao1@gmail.com>
2017-01-22 17:34:43 -0500

    Added new TUT page for android

---
+++ gamejam3/Assets/Tut3Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tut3Controller : MonoBehaviour {
+
+    public Sprite normal,droid;
+
+    // Update is called once per frame
+    void Awake () {
+        if (Application.platform != RuntimePlatform.Android) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+        }
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = droid;
+        }
+
+    }
+}
